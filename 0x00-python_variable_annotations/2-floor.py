9af03496fdd1c62404a2ef7b0128723fac6929e6
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-04 14:14:15 +0000

    Implement `floor()` function with type annotations

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module includes a floor function with type annotations"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Takes one float argument and returns its floor value as integer"""
+    return math.floor(n)
