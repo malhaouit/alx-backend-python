d6fe875cc46711ddb8578baf42e0f1eda5dea545
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-04 13:10:06 +0000

    `add()` function with python annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module includes type-annotated function"""
+
+
+def add(a: float, b: float) -> float:
+    """Takes two float arguments and returns their sum as float"""
+    return a + b
