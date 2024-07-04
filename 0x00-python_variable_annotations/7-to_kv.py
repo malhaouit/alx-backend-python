d2f87640878b41547734aaf256358eff2d62bdc5
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-04 19:28:15 +0000

    Implement `to_kv` function that returns a tuple with values of type string and float

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""The module includes a function with type annotations and returns a tuple"""
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes two different type of arguments and returns a mixed tuple.
+    Parameters:
+    k (str): String
+    v (typing.Union[int, float]): The value can be int or float
+
+    Returns: A tuple in this format (string_value, int_or_float_value)
+    """
+    return (k, float(v))
