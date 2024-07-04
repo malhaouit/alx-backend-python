209c591170693d5bdaab1df7eec845f79863c0cc
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-04 13:55:44 +0000

    `concat()` function with type annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module includes a concatination function with type annotations"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two string arguments and returns their concatination result"""
+    return str1 + str2
