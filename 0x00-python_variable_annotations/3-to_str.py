e458f21e987818e196e8d06d95ff126a1e706aa4
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-04 14:53:07 +0000

    Implement `to_str()` function with type annotations

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module includes a string conversion function with type annotations"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float number to string"""
+    return str(n)
